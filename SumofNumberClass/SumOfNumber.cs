6827fff4836069bbb24f34bb74ff5a64d73fef0a
piyushtambeperficient <61870154+piyushtambeperficient@users.noreply.github.com>
2020-04-02 14:42:10 +0530

    hh

---
+++ SumofNumberClass/SumOfNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumofNumberClass
+{
+    public class SumOfNumber
+    {
+
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+
+        }
+    }
+}
